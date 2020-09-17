83348f34f6106066d38038ded2f2e566763ce00e
Kazi Islam <kislam780@yahoo.com>
2020-09-17 18:46:46 -0500

    File change from Mahmud

--- src/homework/java/mahmud/operators/Assignment_1.java
+++ src/homework/java/mahmud/operators/Assignment_1.java
@@ -2,7 +2,7 @@ package homework.java.mahmud.operators;
 
 public class Assignment_1 {
     public static void main(String[] args) {
-        System.out.println("Hello every one....");
+        System.out.println("Hello every Class....");
 
     }
 }
