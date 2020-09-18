c3905a1c22b7ff7bd164e66a8e6da8ff1652b751
Md Saney Alam <info@compiliaonline.com>
2020-09-17 22:14:47 -0400

    back to old project

--- src/homework/java/aladin/operators/Exercise_5.java
+++
@@ -1,7 +0,0 @@
-package homework.java.aladin.operators;
-
-public class Exercise_5 {
-    public static void main(String[] args) {
-        System.out.println("Hello form Aladin");
-    }
-}
--- src/homework/java/mahmud/variables/Assignment_1.java
+++
@@ -1,7 +0,0 @@
-package homework.java.mahmud.variables;
-
-public class Assignment_1 {
-    public static void main(String[] args) {
-        System.out.println("Hello every one....");
-    }
-}
