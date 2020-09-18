350e7caf71d1f84fc9fc0201da7972d2f51c1d6e
Md Saney Alam <info@compiliaonline.com>
2020-09-17 21:41:37 -0400

    New method added in feature/saney

--- src/homework/java/saney/operators/TestFile.java
+++ src/homework/java/saney/operators/TestFile.java
@@ -1,4 +1,7 @@
 package homework.java.saney.operators;
 
 public class TestFile {
+    public static void main(String[] args) {
+        System.out.println("Hello from Feature/Saney branch");
+    }
 }
