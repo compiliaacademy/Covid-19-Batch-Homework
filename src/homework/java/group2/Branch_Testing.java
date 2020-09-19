56e4851b961aeb52227e9c2b75f139274ea60fc0
Khaled Hasan <khaled_chobi@yahoo.com>
2020-09-18 21:03:55 -0400

    New branch created from Group2

---
+++ src/homework/java/group2/Branch_Testing.java
@@ -0,0 +1,24 @@
+package homework.java.group2;
+
+public class Branch_Testing {
+    public static void main(String args[]){
+
+        // 01. Name,Age and Annual Income.
+
+        String name1, name2, name3, name4, name5;
+        name1 = "Mahmud";
+        name2 = "Shamim";
+        name3 = "Naim";
+        name4 = "Khaled";
+        name5 = "Alauddin";
+
+
+
+        System.out.println("The first attempt to delete branch raised from " + name1 + " Bhai, Who order "
+                + name4 + " to create a branch. " + "Then, " + name2 + ", " + name3 + " and " + name5 + " Bhai agreed with the proposal.");
+
+
+
+    }
+
+}
