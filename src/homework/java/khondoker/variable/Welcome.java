2b64d7463fda2edb5d1188271196b3bed5bf2ead
Khondoker Naimul Islam <naimuap@gmail.com>
2020-09-18 17:14:57 -0700

    Out foulder deleted

--- src/homework/java/khondoker/variable/Welcome.java
+++ src/homework/java/khondoker/variable/Welcome.java
@@ -2,7 +2,7 @@ package homework.java.khondoker.variable;
 
 public class Welcome {
     public static void main  (String args[]){
-        System.out.println("Welcome from Khondoker Naim");
+        System.out.println("Welcome from Khondoker Naimul Islam");
 
 
     }
