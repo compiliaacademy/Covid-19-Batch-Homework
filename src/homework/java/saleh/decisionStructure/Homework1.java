ca9a0896dceb0c1dc0ec9d1ac5ad1c93dffdeef0
Salehahmedbabur <salehahmedbabur@gmail.com>
2020-09-20 22:27:19 -0400

    add

--- src/homework/java/saleh/decisionStructure/Homework1.java
+++ src/homework/java/saleh/decisionStructure/Homework1.java
@@ -4,13 +4,13 @@ public class Homework1 {
     public static void main(String[]args){
         int number;
         Scanner x = new Scanner(System.in);
-        System.out.println("please enter your number");
+        System.out.println("please enter your number#");
         number = x.nextInt();
         if(number % 2 == 0){
-            System.out.println("This is even number");
+            System.out.println("This is even number#");
         } else
         {
-            System.out.println("This is odd number");
+            System.out.println("This is odd number#");
         }
 
     }
