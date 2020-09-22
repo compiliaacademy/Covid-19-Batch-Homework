32d9ea63fac7d1560dd878c03539e82d1f89915f
Khondoker Naimul Islam <naimuap@gmail.com>
2020-09-21 17:50:10 -0700

    Dicision structure homework upload

---
+++ src/homework/java/khondoker/decisoon/Assignment/Exercise1Odd.java
@@ -0,0 +1,13 @@
+package homework.java.khondoker.decisoon.Assignment;
+public class Exercise1Odd {
+    public static void main(String args[]) {
+        int num = 5;
+        if (num % 2 == 0) {
+        System.out.println("Entered number is even");}
+        else{
+        System.out.println("Entered number is odd");}
+
+        System.out.println("Bye");
+    }
+
+}
---
+++ src/homework/java/khondoker/decisoon/Assignment/PrintNumberInWord.java
@@ -0,0 +1,9 @@
+package homework.java.khondoker.decisoon.Assignment;
+public class PrintNumberInWord {
+    public static void main(String[] args) {
+        int number5=5;
+       if (number5 == 5)
+            System.out.println("FIVE");
+
+    }
+}
