04498ff9526920247c90153c34b8d59b207c0d2f
SALMAN <RazoN@Salmans-MacBook-Air.local>
2020-09-22 22:28:15 -0400

    Create a new branch Feature/Salman

---
+++ src/homework/java/salman/decision/structure/largest_number.java
@@ -0,0 +1,25 @@
+package homework.java.salman.decision.structure;
+
+public class largest_number {
+    public static void main (String []args) {
+
+    int num1=25, num2=77,num3=96,num4=125,num5=2;
+
+    if ((num1>num2) && (num1>num3) && (num1>num4) && (num1>num5)){
+            System.out.println("num1 is the largest number");}
+
+    else if ((num2>num1) && (num2>num3) && (num2>num4) && (num2>num5)){
+            System.out.println("num2 is the largest number");}
+
+    else if ((num3>num1) && (num3>num2) && (num3>num4) && (num3>num5)){
+            System.out.println("num3 is the largest number");}
+
+    else if ((num4>num1) && (num4>num3) && (num4>num3) && (num4>num5)){
+            System.out.println("num4 is the largest number");}
+
+    else
+            System.out.println("num5 is the largest");
+
+    }
+
+}
