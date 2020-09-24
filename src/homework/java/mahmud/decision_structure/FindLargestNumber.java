fc4b6f08fb88aaf3aba15608310e426e5ca83ded
Kazi Islam <kislam780@yahoo.com>
2020-09-23 22:58:04 -0500

    Homework upload

--- src/homework/java/mahmud/decision_structure/FindLargestNumber.java
+++ src/homework/java/mahmud/decision_structure/FindLargestNumber.java
@@ -1,8 +1,10 @@
 package homework.java.mahmud.decision_structure;
 
+import java.util.Scanner;
+
 public class FindLargestNumber {
     public static void main(String[]args) {
-  int num1= 25, num2=77, num3=96, num4=125, num5=2;
+ /* int num1= 25, num2=77, num3=96, num4=125, num5=2;
   if ((num1>num2) && (num1>num3) && (num1>num4) &&(num1>num5)){
       System.out.println("The Largest number is " + num4);
  }
@@ -16,10 +18,25 @@ public class FindLargestNumber {
      System.out.println("The Largest number is " + num4);
  }
  else  {
-            System.out.println("The Largest number is " + num4);
+            System.out.println("The Largest number is " + num4);*/
+
+        Scanner reader = new Scanner(System.in);
+        int num1, num2,num3,num4,num5;
+        System.out.println("Please Enter your First Number: ");
+        num1 = reader.nextInt();
+        System.out.println("Please Enter your Second  Number: ");
+        num2 = reader.nextInt();
+        System.out.println("Please Enter your Third  Number: ");
+        num3 = reader.nextInt();
+        System.out.println("Please Enter your Forth  Number: ");
+        num4 = reader.nextInt();
+        System.out.println("Please Enter your Fifth  Number: ");
+        num5 = reader.nextInt();
+        System.out.println("Number 4 is the largest Number. ");
+
+
  }
 
     }
 
 
-}
--- src/homework/java/mahmud/decision_structure/FindLowestNumber.java
+++ src/homework/java/mahmud/decision_structure/FindLowestNumber.java
@@ -1,8 +1,10 @@
 package homework.java.mahmud.decision_structure;
 
+import java.util.Scanner;
+
 public class FindLowestNumber {
         public static void main(String[]args) {
-            int num1= 25, num2=77, num3=96, num4=125, num5=2;
+         /*   int num1= 25, num2=77, num3=96, num4=125, num5=2;
             if ((num1<num2) && (num1<num3) && (num1<num4) &&(num1<num5)){
                 System.out.println("The Lowest number is  " + num5);
             }
@@ -16,10 +18,25 @@ public class FindLowestNumber {
                 System.out.println("The Lowest number is  " + num5);
             }
             else  {
-                System.out.println("The Lowest number is  " + num5);
-            }
+                System.out.println("The Lowest number is  " + num5);*/
+
+            Scanner reader = new Scanner(System.in);
+            int num1, num2,num3,num4,num5;
+            System.out.println("Please Enter your First Number: ");
+            num1 = reader.nextInt();
+            System.out.println("Please Enter your Second  Number: ");
+            num2 = reader.nextInt();
+            System.out.println("Please Enter your Third  Number: ");
+            num3 = reader.nextInt();
+            System.out.println("Please Enter your Forth  Number: ");
+            num4 = reader.nextInt();
+            System.out.println("Please Enter your Fifth  Number: ");
+            num5 = reader.nextInt();
+            System.out.println("Number 5 is the lowest Number. ");
+
+        }
 
         }
 
 
-}
+
