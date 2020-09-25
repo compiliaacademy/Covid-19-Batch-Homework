9d29556fd4a371484edb884dd4336fc327836e7a
S. M. Mahiuddin Shamim <mahiuddins27@gmail.com>
2020-09-24 19:04:02 -0500

    homework submitted from Shamim

---
+++ src/homework/java/shamim/Decision/Homework.java
@@ -0,0 +1,103 @@
+package homework.java.shamim.Decision;
+
+public class Homework {
+
+    public static void main(String[] args) {
+
+        //Exercise 1
+
+        System.out.println("Exercise 1:");
+        int number = 5;
+        if (number / 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+        System.out.println("BYE");
+        System.out.println(" ");
+        System.out.println(" ");
+
+
+        int Number = 10;
+        if (Number % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+        System.out.println("BYE");
+        System.out.println(" ");
+        System.out.println(" ");
+
+
+        // Exercise 2
+
+        System.out.println("Exercise 2:");
+
+        int digit = 5;
+        if (digit == 0) {
+            System.out.println("Zero");
+        } else if (digit == 1) {
+            System.out.println("One");
+        } else if (digit == 2) {
+            System.out.println("Two");
+        } else if (digit == 3) {
+            System.out.println("Three");
+        } else if (digit == 4) {
+            System.out.println("Four");
+        } else if (digit == 5) {
+            System.out.println("Five");
+        } else if (digit == 6) {
+            System.out.println("Six");
+        } else if (digit == 7) {
+            System.out.println("Seven");
+        } else if (digit == 8) {
+            System.out.println("Eight");
+        } else if (digit == 9) {
+            System.out.println("Nine");
+        } else {
+            System.out.println("Others");
+        }
+        System.out.println("Thank You");
+        System.out.println(" ");
+        System.out.println(" ");
+
+
+        // Exercise 3
+
+        System.out.println("Exercise 3:");
+        int num1 = 25, num2 = 77, num3 = 96, num4 = 125, num5 = 2;
+        if ((num1 > num2) && (num1 > num3) && (num1 > num4) && (num1 > num5)) {
+            System.out.println("The largest number is 25"); }
+        else if ((num2 > num1) && (num2 > num3) && (num2 > num4) && (num2 > num5)) {
+            System.out.println("The largest number is 77"); }
+        else if ((num3 > num1) && (num3 > num2) && (num3 > num4) && (num3 > num5)) {
+            System.out.println("The largest number is 96"); }
+        else if ((num4 > num1) && (num4 > num2) && (num4 > num3) && (num4 > num5)) {
+            System.out.println("The largest number is 125"); }
+        else {
+            System.out.println("The largest number is 2"); }
+        System.out.println(" ");
+        System.out.println(" ");
+
+
+        // Exercise 4
+
+        //int num1 = 25, num2 = 77, num3 = 96, num4 = 125, num5 = 2;
+        System.out.println("Exercise 4:");
+        if ((num1 < num2) && (num1 < num3) && (num1 < num4) && (num1 < num5)) {
+            System.out.println("The lowest number is 25"); }
+        else if ((num2 < num1) && (num2 < num3) && (num2 < num4) && (num2 < num5)) {
+            System.out.println("The lowest number is 77"); }
+        else if ((num3 < num1) && (num3 < num2) && (num3 < num4) && (num3 < num5)) {
+            System.out.println("The lowest number is 96"); }
+        else if ((num4 < num1) && (num4 < num2) && (num4 < num3) && (num4 < num5)) {
+            System.out.println("The lowest number is 125"); }
+        else {
+            System.out.println("The lowest number is 2"); }
+        System.out.println(" ");
+        System.out.println(" ");System.out.println("Exercise 1:");
+
+
+
+    }
+}
