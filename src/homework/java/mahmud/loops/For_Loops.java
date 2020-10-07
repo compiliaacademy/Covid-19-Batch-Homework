1c43ac123b8496fa05fb92847a9ae084f5d4df18
Kazi Islam <kislam780@yahoo.com>
2020-10-07 00:58:17 -0500

    Little modify for looping Time Table

--- src/homework/java/mahmud/loops/For_Loops.java
+++ src/homework/java/mahmud/loops/For_Loops.java
@@ -3,9 +3,12 @@ package homework.java.mahmud.loops;
 public class For_Loops {
     public static void main(String[] args) {
 
+        System.out.println( "****Well Come To Time Table****");
         int n = 4;
         for (int i = 1; i <= 10; i++) {
             System.out.println(n + " X " + i + " = " + n * i);
+
+            System.out.println( "****Thank You****");
 
 
         }
