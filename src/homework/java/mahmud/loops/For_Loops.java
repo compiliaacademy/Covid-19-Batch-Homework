d018a196bfa74846e5e8fe1321ba973348101409
Kazi Islam <kislam780@yahoo.com>
2020-10-06 23:12:36 -0500

    New home work update for loops Time table.

---
+++ src/homework/java/mahmud/loops/For_Loops.java
@@ -0,0 +1,14 @@
+package homework.java.mahmud.loops;
+
+public class For_Loops {
+    public static void main(String[] args) {
+
+        int n = 4;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " X " + i + " = " + n * i);
+
+
+        }
+
+    }
+}
---
+++ src/homework/java/mahmud/loops/For_Loops_Scanner.java
@@ -0,0 +1,18 @@
+package homework.java.mahmud.loops;
+
+import java.util.Scanner;
+
+public class For_Loops_Scanner {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num=0;
+        System.out.println( "Please Enter your First Number");
+        num = input.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+        System.out.println(num + " x " + i + " = " + num * i);
+
+        }
+    }
+}
