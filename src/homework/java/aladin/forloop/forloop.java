83f6cb72ab4c98d4c3220f1a0e1d8932ee9d4a3f
MohammedAlauddin <uddin9857@gmail.com>
2020-10-07 03:22:38 -0400

    feature/aladin

---
+++ src/homework/java/aladin/forloop/forloop.java
@@ -0,0 +1,44 @@
+package homework.java.aladin.forloop;
+
+import java.sql.SQLOutput;
+
+public class forloop {
+    public static void main(String[] args) {
+        System.out.println("I am a Soccer player");
+         System.out.println("I am a Soccer player");
+        System.out.println("I am a Soccer player");
+         System.out.println("I am a Soccer player");
+         System.out.println("I am a Soccer player");
+
+        System.out.println("---------------------");
+
+        // To Print multiple times in one line coding
+         for(int table = 1; table<=7;table ++) {
+             System.out.println("I love Bangladesh");
+         }
+
+        System.out.println("---------------------");
+
+        for(int table = 1; table<=12;table ++) {
+            System.out.println(table);
+        }
+
+        System.out.println("---------------------");
+
+        // Multiplication table which is Namta.
+            for(int table = 1; table<=10;table ++) {
+                System.out.println(table + "*" + 1 + "=" + (table * 1));
+            }
+
+        System.out.println("---------------------");
+            
+            // Multiplication again.
+
+                for(int number = 1; number<=10; number ++) {
+                    System.out.println(1 + "*" + number + "=" + (number));
+                }
+
+
+
+    }
+}
